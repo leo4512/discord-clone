5613120ce7fdfb150dab01352e241bbd194d1b2d
leo <leoyh.97@gmail.com>
2023-11-02 14:49:50 +0800

    basic user auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
 		<div>
-			<p className="text-3xl font-bold text-indigo-500">hello</p>
-      <Button>Click me</Button>
+			<UserButton 
+				afterSignOutUrl="/"
+			/>
 		</div>
 	);
 }
