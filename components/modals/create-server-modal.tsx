2cc0b6e3615ac4ddba211ab54f8497d04120864b
leo <leoyh.97@gmail.com>
2023-11-10 14:50:56 +0800

    Completed invitation modal, invitation link copy, and invitation link generation.

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -74,7 +74,7 @@ export const CreateServerModal = () => {
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
 				<DialogHeader className="pt-8 px-6">
 					<DialogTitle className="text-2xl text-center font-bold">
-						Create new server
+						Create New Server
 					</DialogTitle>
 					<DialogDescription className="text-center text-zinc-500">
 						Give your server some personality with a name and an image. You can
