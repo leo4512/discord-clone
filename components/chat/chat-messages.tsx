6435d35c063130f083dcb0c0f6216762e568ba35
leo <leoyh.97@gmail.com>
2023-11-19 17:40:56 +0800

    Completed message rendering and message edit/delete feature.

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { Member, Message, Profile } from "@prisma/client";
-import { ChatWelcome } from "@/components/chat/chat-welcome";
+
+import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
+
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
 	member: Member & {
@@ -76,7 +83,19 @@ export const ChatMessages = ({
 				{data?.pages?.map((group, i) => (
 					<Fragment key={i}>
 						{group.items.map((message: MessageWithMemberWithProfile) => (
-							<div key={message.id}>{message.content}</div>
+							<ChatItem
+								key={message.id}
+								id={message.id}
+								content={message.content}
+								member={message.member}
+								timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+								fileUrl={message.fileUrl}
+								deleted={message.deleted}
+								currentMember={member}
+								isUpdated={message.updatedAt !== message.createdAt}
+								socketUrl={socketUrl}
+								socketQuery={socketQuery}
+							/>
 						))}
 					</Fragment>
 				))}
