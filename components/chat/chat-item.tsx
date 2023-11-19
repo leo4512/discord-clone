266c61fb0ad0f449f1df22e798470b1cc5592c59
leo <leoyh.97@gmail.com>
2023-11-19 18:03:00 +0800

    Added redirection to 1-1 chat when clicking user profile pic or name in channels.

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
 import { useModal } from "@/hooks/use-modal-store";
+import { useRouter, useParams } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
@@ -59,6 +60,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
 	const [isEditing, setIsEditing] = useState(false);
 	const { onOpen } = useModal();
+	const params = useParams();
+	const router = useRouter();
+
+	const onMemberClick = () => {
+		if (member.id === currentMember.id) {
+			return;
+		}
+
+		router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+	};
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -118,13 +129,19 @@ export const ChatItem = ({
 	return (
 		<div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
 			<div className="group flex gap-x-2 items-start w-full">
-				<div className="cursor-pointer hover:drop-shadow-md transition">
+				<div
+					onClick={onMemberClick}
+					className="cursor-pointer hover:drop-shadow-md transition"
+				>
 					<UserAvatar src={member.profile.imageUrl} />
 				</div>
 				<div className="flex flex-col w-full">
 					<div className="flex items-center gap-x-2">
 						<div className="flex items-center">
-							<p className="font-semibold text-sm hover:underline cursor-pointer">
+							<p
+								onClick={onMemberClick}
+								className="font-semibold text-sm hover:underline cursor-pointer"
+							>
 								{member.profile.name}
 							</p>
 							<ActionTooltip label={member.role}>
